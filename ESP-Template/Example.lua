2a6a1db9c5c3e94acc8f1bd2014216249d2e9963
scripterjauy <jauy4528n@gmail.com>
2026-04-01 13:33:50 +0700

    Update Example.lua

--- ESP-Template/Example.lua
+++ ESP-Template/Example.lua
@@ -3,7 +3,7 @@
 -- You can look for the new example in readme.md
 
 
-local espLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/scripterpan/scripterpan/refs/heads/main/ESP-Template/Script.lua"))()
+local espLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/scripterjauy/Backup/refs/heads/main/ESP-Template/Source.lua"))()
 
 local target = workspace:WaitForChild("Example")  -- Change to the model you want to highlight
 local color = Color3.fromRGB(255, 0, 0)        -- Change to your preferred color
